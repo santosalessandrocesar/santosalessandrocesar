9fa8361db20bc3688976b304562ea65c0cf4e097
Alessandro Cesar <81247428+santosalessandrocesar@users.noreply.github.com>
2022-11-16 16:03:31 -0300

    Update copy_groups.ps1

--- scripts-active-directory/copy_groups.ps1
+++ scripts-active-directory/copy_groups.ps1
@@ -2,3 +2,5 @@
 
 # Subistitua ORIGEM e DESTINO pelo SAMAccountName do usuário. // Replace SOURCE and DESTINATION with the user's SAMAccountName.
 # O DESTINO receberá a cópia dos grupos da ORIGEM // DESTINATION will receive a copy of the ORIGIN groups
+
+#Script By FF
